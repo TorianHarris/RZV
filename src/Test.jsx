085aa83e4b3714b629a0a9a5e2f22732d6830d31
Shaun <torianharris917@gmail.com>
2019-10-13 18:32:11 -0400

    feat/backend set-up

---
+++ src/Test.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Test extends Component {
+  render() {
+    return <div>Hello this is the Test.</div>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import App from "./App";
+import Test from "./Test";
+import DatePicker from './DatePicker';
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green
+  }
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <DatePicker />
+    <Test />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
